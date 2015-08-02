c23e14241722616aa402ac0daeb0eac562fb34d6
jorhett <jrhett@netconsonance.com>
2015-08-02 21:35:51 +0000

    Recent changes from testing

--- manifests/init.pp
+++ manifests/init.pp
@@ -45,20 +45,20 @@
 #
 # manifests/init.pp
 class puppet4(
-  # common variables for all Puppet4 classes
-  Integer $collection_version = 1,
-  Boolean $repo_enabled = true,
-  String $loglevel = 'warning',
-) {
+  Integer $collection_version = $::puppet4::params::collection_version,
+  Enum['0','1'] $repo_enabled = $::puppet4::params::repo_enabled,
+  String $loglevel            = $::puppet4::params::loglevel,
+)
+inherits puppet4::params {
   
-  if( $facts['os']['family'] == 'RedHat' ) {
-    yumrepo { "puppetlabs-pc${collection_version}":
-      ensure   => 'present',
-      baseurl  => "http://yum.puppetlabs.com/el/7/PC${collection_version}/\$basearch",
-      descr    => "Puppet Labs PC${collection_version} Repository EL ${facts['os']['release']['major']} - \$basearch",
-      enabled  => $repo_enabled,
-      gpgcheck => '1',
-      gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-puppetlabs',
-    }
-  }
+#  if( $facts['os']['family'] == 'RedHat' ) {
+#    yumrepo { "puppetlabs-pc${collection_version}":
+#      ensure   => 'present',
+#      baseurl  => "http://yum.puppetlabs.com/el/7/PC${collection_version}/\$basearch",
+#      descr    => "Puppet Labs PC${collection_version} Repository EL ${facts['os']['release']['major']} - \$basearch",
+#      enabled  => $repo_enabled,
+#      gpgcheck => '1',
+#      gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-puppetlabs',
+#    }
+#  }
 }
---
+++ manifests/params.pp
@@ -0,0 +1,5 @@
+class puppet4::params() {
+  $collection_version = 1
+  $repo_enabled       = '1'
+  $loglevel           = 'warning'
+}
